a2335538542f5889b24eb062009e69f04cb5245f
jumpingfrog0 <jumpingfrog0@gmail.com>
2024-01-27 02:40:33 +0800

    ObjcDemo

---
+++ ObjcDemo/ObjcDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjcDemo
+//
+//  Created by 黄东鸿 on 2024/1/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjcDemo/ObjcDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ObjcDemo
+//
+//  Created by 黄东鸿 on 2024/1/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
