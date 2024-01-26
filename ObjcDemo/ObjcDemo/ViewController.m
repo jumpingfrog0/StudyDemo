004dc8a28de827f5e843270544e0e84564b7c6f5
jumpingfrog0 <jumpingfrog0@gmail.com>
2024-01-27 03:04:53 +0800

    内存对齐

--- ObjcDemo/ObjcDemo/ViewController.m
+++ ObjcDemo/ObjcDemo/ViewController.m
@@ -7,6 +7,31 @@
 
 #import "ViewController.h"
 
+struct Struct0 {
+    int c;
+}s0;
+
+struct Struct1 {
+    double a;
+    char b;
+    int c;
+    short d;
+}s1;
+
+struct Struct3 {
+    double a;           // 8    [0，7]
+    int b;              // 4    [8，11]
+    char c;             // 1    [12]
+    short d;            // 2    (13, [14，15]
+    int e;              // 4    [16, 19]
+    struct Struct3_1 {
+        double a;       // 8    (20, 21, 22, 23, [24, 31]
+        char b;         // 1    [32]
+        int c;          // 4    (33, 34, 35 [36, 39]
+        short d;        // 2    [40, 41]，42, 43, 44, 45, 46, 47, 48
+    }s1;
+}s3;
+
 @interface ViewController ()
 
 @end
@@ -15,7 +40,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // 内存字节对齐
+    printf("Struct1 size: %lu\n", sizeof(s0));  // 输出 4
+    printf("Struct1 size: %lu\n", sizeof(s1));  // 输出 24
+    printf("Struct1 size: %lu\n", sizeof(s3));  // 输出 48
 }
 
 
